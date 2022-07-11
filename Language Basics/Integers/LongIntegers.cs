8a0a6c390fd58ca56199e9af192fdf5a33e7680b
Vermesheleva <boken1095@tut.by>
2022-07-12 02:07:11 +0300

    add some tasks

---
+++ Language Basics/Integers/LongIntegers.cs
@@ -0,0 +1,65 @@
+﻿namespace Literals
+{
+    public static class LongIntegers
+    {
+        public static long ReturnLongInteger21()
+        {
+            return 4_956_185_095_298_947_214L;
+        }
+
+        public static long ReturnLongInteger22()
+        {
+            return -1_280_010_762_458_239_942L;
+        }
+
+        public static long ReturnLongInteger23()
+        {
+            return -945_783_496_234_828_465L;
+        }
+
+        public static ulong ReturnLongInteger24()
+        {
+           return 16_269_823_234_523_742_845uL;
+        }
+
+        public static long ReturnLongInteger25()
+        {
+            return 0x7FFF_FFFF_FFFF_FFFFL;
+        }
+
+        public static long ReturnLongInteger26()
+        {
+            return 0x0ABCDEF_ABCDE_FABCL;
+        }
+
+        public static ulong ReturnLongInteger27()
+        {
+            return 0xF97C_39E7_17FA_63BDuL;
+        }
+
+        public static ulong ReturnLongInteger28()
+        {
+            return 0xC4F7_E62B_7A90_A0BFuL;
+        }
+
+        public static long ReturnLongInteger29()
+        {
+            return 0b111000_11101000_11010101_00111010_10111010_01101010_10011010_10010101L;
+        }
+
+        public static long ReturnLongInteger210()
+        {
+           return 0b10110_11010100_10010100_00010101_01110111_01001111_00001110_10010101L;
+        }
+
+        public static long ReturnLongInteger211()
+        {
+            return 0b1010101_01010101_01010101_01010101_01010101_01010101_01010101_01010101L;
+        }
+
+        public static long ReturnLongInteger212()
+        {
+            return 0b1110101_00111000_11011100_11111011_01110110_00010111_11111111_11111111L;
+        }
+    }
+}
